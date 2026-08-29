c517046d6d8bcb47e2fde2e7a9fba96ee6eb5c5d
lmj <674494205@qq.com>
2026-08-29 11:44:05 +0800

    feat: 项目初始化

---
+++ src/main/java/com/xingyun/template/Application.java
@@ -0,0 +1,18 @@
+package com.xingyun.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动类
+ *
+ * @author bulinZzz
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
